d3440855ca3f27fdf1325186da71b41a64ac71b8
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-07-24 15:33:35 +0700

    X00TD: Bringup to @DerpFest-AOSP
    Signed-off-by: ikwfahmi <mikhwanfahmi4@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The DerpFest Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/derp_X00TD.mk
